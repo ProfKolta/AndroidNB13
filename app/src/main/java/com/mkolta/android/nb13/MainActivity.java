d88b97e50d86fe4d5f2062e7d5151073aa691b13
ProfKolta <mkolta@calbaptist.edu>
2018-07-07 11:36:14 -0700

    added color and edit text widget

--- app/src/main/java/com/mkolta/android/nb13/MainActivity.java
+++ app/src/main/java/com/mkolta/android/nb13/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.RelativeLayout;
 import android.widget.Button;
+import android.graphics.Color;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +13,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //layout
         RelativeLayout mRelativeLayout = new RelativeLayout(this);
-        Button mRedButton = new Button(this);
+        mRelativeLayout.setBackgroundColor(Color.GREEN);
 
-        mRelativeLayout.addView(mRedButton);
+        //button
+        Button mRedButton = new Button(this);
+        mRedButton.setText("Click here");
+        mRedButton.setBackgroundColor(Color.RED);
+        mRedButton.setId(1);
+
+        //EditText
+        EditText mUserName = new EditText(this);
+        mUserName.setId(2);
+
+        RelativeLayout.LayoutParams buttonDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+
+        RelativeLayout.LayoutParams userNameDetails = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        userNameDetails.addRule(RelativeLayout.ABOVE, mRedButton.getId());
+        userNameDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        userNameDetails.setMargins(0, 0, 0, 50);
+
+        buttonDetails.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        buttonDetails.addRule(RelativeLayout.CENTER_VERTICAL);
+
+
+
+
+        mRelativeLayout.addView(mRedButton, buttonDetails);
+        mRelativeLayout.addView(mUserName, userNameDetails);
 
         setContentView(mRelativeLayout);
     }
