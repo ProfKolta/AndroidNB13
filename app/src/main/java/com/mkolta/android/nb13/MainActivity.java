a59040219af1341d1834672e124217dcfb2da955
ProfKolta <mkolta@calbaptist.edu>
2018-07-06 17:56:17 -0700

    added relative layout and button

--- app/src/main/java/com/mkolta/android/nb13/MainActivity.java
+++ app/src/main/java/com/mkolta/android/nb13/MainActivity.java
@@ -2,12 +2,22 @@ package com.mkolta.android.nb13;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        RelativeLayout mRelativeLayout = new RelativeLayout(this);
+        Button mRedButton = new Button(this);
+
+        mRelativeLayout.addView(mRedButton);
+
+        setContentView(mRelativeLayout);
     }
+
+
 }
